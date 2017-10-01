560539bdda6bbda70db4f88f9d475c6de5bf2ce8
EC2 Default User <ec2-user@ip-172-31-41-125.eu-west-1.compute.internal>
2017-10-01 17:07:18 +0000

    feat(update): update docker

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 cd /home/ec2-user/pushmyshop-docker;
-/usr/local/bin/docker-compose/docker-compose pull;
-/usr/local/bin/docker-compose/docker-compose up -d;
+/usr/local/bin/docker-compose pull;
+/usr/local/bin/docker-compose up -d;
